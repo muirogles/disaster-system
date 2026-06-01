74a7cd3ca1fb6ac593eea2d05d25cc32c665e136
María Rogles <muirogles@gmail.com>
2026-06-01 15:18:26 +0200

    feat: multievent qrs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Use relative paths for assets for maximum flexibility
-  base: './',
+  base: '/disaster-system/',
   build: {
     outDir: 'dist',
     // Ensure all assets are included
